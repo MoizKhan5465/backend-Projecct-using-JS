35cc122aba10889070837a73ed425ca4b9e0e95e
MoizKhan5465 <moizsardar056@gmail.com>
2026-08-08 03:22:27 +0500

    register control addded

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,57 @@
+import { User } from "../models/user.model.js";
+import { APIerror } from "../utils/APIerror.js";
+import { APIresponse } from "../utils/APIresponse.js";
+import { asyncHandler } from "../utils/asynchandler.js";
+import { uploadonclodinary } from "../utils/Cloudnary.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullname, email, password, username } = req.body;
+  console.log(email);
+  if (!fullname || !email || !password || !username) {
+    throw new APIerror(400, "Please provide all required fields");
+  }
+
+  const existeduser = User.findOne({ $or: [{ username }, { email }] });
+  if (existeduser) {
+    throw new APIerror(409, "User already exists with this username or email");
+  }
+
+  const avtarlocalpath = req.files?.avtar[0]?.path;
+  const coverimagelocalpath = req.files?.coverimage[0]?.path;
+
+  if (!avtarlocalpath) {
+    throw new APIerror(400, "Please provide  avatar ");
+  }
+
+  const avtarupload = await uploadonclodinary(avtarlocalpath);
+
+  const coverimageupload = coverimagelocalpath
+    ? await uploadonclodinary(coverimagelocalpath)
+    : null;
+  if (!avtarupload) {
+    throw new APIerror(500, "Failed to upload avatar to Cloudinary");
+  }
+
+  const usercreation = await User.create({
+    fullname,
+    avtar: avtarupload.url,
+    coverimage: coverimageupload?.url || null,
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const creaateduser = await usercreation
+    .findbyId(usercreation._id)
+    .select("-password -refreshToken");
+
+  if (!creaateduser) {
+    throw new APIerror(500, "Failed to create user");
+  }
+
+  return res
+    .status(201)
+    .json(new APIresponse(200, createduser, "User created successfully"));
+});
+
+export { registerUser };
--- src/utils/APIresponse.js
+++ src/utils/APIresponse.js
@@ -6,3 +6,5 @@ class APIresponse {
     this.success = statusCode >= 200 && statusCode < 300;
   }
 }
+
+export { APIresponse };
